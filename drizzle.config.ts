d42f80db444d9084ce2baf34ea493c46c5cc82b2
Arun Ravindran <39104424+arun-1993@users.noreply.github.com>
2025-02-15 13:44:35 +0530

    feat: set up db integration with singlestore

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,14 +1,18 @@
-import { type Config } from "drizzle-kit";
-
+import { defineConfig, type Config } from "drizzle-kit";
 import { env } from "~/env";
 
-const config = {
+const config = defineConfig({
 	schema: "./src/server/db/schema.ts",
-	dialect: "sqlite",
-	dbCredentials: {
-		url: env.DATABASE_URL,
-	},
+	dialect: "singlestore",
 	tablesFilter: ["cloud-file-manager_*"],
-} satisfies Config;
+	dbCredentials: {
+		host: env.SINGLESTORE_HOST,
+		port: parseInt(env.SINGLESTORE_PORT),
+		user: env.SINGLESTORE_USER,
+		password: env.SINGLESTORE_PASS,
+		database: env.SINGLESTORE_DB,
+		ssl: {},
+	},
+} satisfies Config);
 
 export default config;
