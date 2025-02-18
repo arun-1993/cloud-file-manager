23b4d8f84d4bd0dbc70893d20bb322e76d443f84
Arun Ravindran <39104424+arun-1993@users.noreply.github.com>
2025-02-19 02:39:09 +0530

    feat: modify database schema
    - set prefix for all table names
    - add schema for folders and files tables
    - update schema for users table

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,12 +1,46 @@
 import {
 	bigint,
-	int,
-	singlestoreTable,
+	index,
+	singlestoreTableCreator,
 	text,
+	tinyint,
 } from "drizzle-orm/singlestore-core";
 
-export const users = singlestoreTable("users_table", {
-	id: bigint("id", { mode: "bigint" }).primaryKey().autoincrement(),
+export const createTable = singlestoreTableCreator(
+	(name) => `cloud-file-manager_${name}`,
+);
+
+export const files = createTable(
+	"files",
+	{
+		id: bigint("id", { mode: "number", unsigned: true })
+			.primaryKey()
+			.autoincrement(),
+		name: text("name").notNull(),
+		parent: bigint("parent", { mode: "number", unsigned: true }).notNull(),
+		size: bigint("size", { mode: "number", unsigned: true }).notNull(),
+		url: text("url").notNull(),
+	},
+	(table) => [index("parent_index").on(table.parent)],
+);
+
+export const folders = createTable(
+	"folders",
+	{
+		id: bigint("id", { mode: "number", unsigned: true })
+			.notNull()
+			.primaryKey()
+			.autoincrement(),
+		name: text("name").notNull(),
+		parent: bigint("parent", { mode: "number", unsigned: true }),
+	},
+	(table) => [index("parent_index").on(table.parent)],
+);
+
+export const users = createTable("users", {
+	id: bigint("id", { mode: "number", unsigned: true })
+		.primaryKey()
+		.autoincrement(),
 	name: text("name"),
-	age: int("age"),
+	age: tinyint("age"),
 });
